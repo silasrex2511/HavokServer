6e4f64156a31fef5647b2662e8a058b82774d1b4
silasrex2511 <silasrex2511@gmail.com>
2015-07-02 01:00:16 -0500

    working server

--- Main/JS/game.js
+++ Main/JS/game.js
@@ -17,50 +17,51 @@ function render() {
     ctx.fillStyle = "BLACK";
     ctx.fillRect(0,0,c.width,c.height);
     update();
-    // itemsShow(players);
-
+    itemsShow(players);
 }
 
 function update(){
-  // if(playerLocal.up){
-  //   player.y -= speed;
-  // }
-  // if(playerLocal.down){
-  //   player.y += speed;
-  // }
-  // if(playerLocal.right){
-  //   player.x += speed;
-  // }
-  // if(playerLocal.left){
-  //   player.x -= speed;
-  // }
+  keyInput(playerLocal);
+  if(playerLocal.up){
+    playerLocal.y -= speed;
+  }
+  if(playerLocal.down){
+    playerLocal.y += speed;
+  }
+  if(playerLocal.right){
+    playerLocal.x += speed;
+  }
+  if(playerLocal.left){
+    playerLocal.x -= speed;
+  }
+  worldCol(playerLocal,"wall");
 }
 
 //misc functions
-// function keyInput(player){
-//   kd.A.down(function(){
-//     player.left = true;
-//   });
-//   kd.A.up(function(){
-//     player.left = false;
-//   });
-//   kd.S.down(function(){
-//     player.down = true;
-//   });
-//   kd.S.up(function(){
-//     player.down = false;
-//   });
-//   kd.D.down(function(){
-//     player.right = true;
-//   });
-//   kd.D.up(function(){
-//     player.right = false;
-//   });
-//   kd.W.down(function(){expression
-//     player.up = true;
-//   });
-//   kd.W.up(function(){
-//     player.up = false;
-//   });
-//   kd.tick();
-// }
+function keyInput(player){
+  kd.A.down(function(){
+    player.left = true;
+  });
+  kd.A.up(function(){
+    player.left = false;
+  });
+  kd.S.down(function(){
+    player.down = true;
+  });
+  kd.S.up(function(){
+    player.down = false;
+  });
+  kd.D.down(function(){
+    player.right = true;
+  });
+  kd.D.up(function(){
+    player.right = false;
+  });
+  kd.W.down(function(){
+    player.up = true;
+  });
+  kd.W.up(function(){
+    player.up = false;
+  });
+  kd.tick();
+}
--- NJS/app.js
+++ NJS/app.js
@@ -1,20 +1,28 @@
 var http = require('http').createServer(handler)
 var io = require('socket.io')(http);
 var fs = require('fs');
+var mime = require('mime');
+
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/../Main/index.html',
-  function (err, data) {
-    if (err) {
+  if(req.url !== "/favicon.ico"){
+    base = __dirname + "/../Main"+ req.url;
+  }else{
+    base = __dirname + req.url;
+  }
+  fs.readFile(base,function (err, data) {
+    if (!err) {
+      res.writeHead(200,{"content-type":mime.lookup(req.url)});
+      res.end(data);
+    }else{
       res.writeHead(500);
-      return res.end('Error loading index.html');
+      return res.end('Error loading index.html\n' + base);
+      console.log(base);
     }
-
-    res.writeHead(200);
-    res.end(data);
   });
 }
 
+//will be moved to a separate module
 io.on('connection', function(socket){
   console.log("a user connected");
   socket.on('disconnect', function(){
